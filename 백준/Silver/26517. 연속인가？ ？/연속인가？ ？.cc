f69dc85e9bab7b68890291a402dc234b0b62b7b5
dudurim <124253892+dudu1021@users.noreply.github.com>
2024-07-03 10:05:50 +0900

    [Silver V] Title: 연속인가? ?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.cc
+++ 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.cc
@@ -6,8 +6,8 @@ int main() {
     long long k, a, b, c, d;
     cin >> k >> a >> b >> c >> d;
     if(a*k+b == c*k+d)
-        cout << "Yes " << a*k+b << "\n" ;
+        cout << "Yes " << a*k+b;
     else
-        cout << "No" << "\n";
+        cout << "No";
     return 0;
 }
