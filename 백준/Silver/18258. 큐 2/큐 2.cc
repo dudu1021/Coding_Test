fda41623bfe9cc0a9fc328cca87b0d41545363f8
dudurim <124253892+dudu1021@users.noreply.github.com>
2024-07-16 15:25:29 +0900

    [Silver IV] Title: 큐 2, Time: 384 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    queue<int> Q;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        string q;
+        cin >> q;
+        if (q == "push") {
+            int val;
+            cin >> val;
+            Q.push(val);
+        }
+        else if (q == "pop") {
+            if (Q.empty()) cout << -1 << '\n';
+            else {
+                cout << Q.front() << '\n';
+                Q.pop();
+            }
+        }
+        else if (q == "size") {
+            cout << Q.size() << '\n';
+        }
+        else if (q == "empty") {
+            cout << Q.empty() << '\n';
+        }
+        else if (q == "front") {
+            if (Q.empty()) cout << -1 << '\n';
+            else cout << Q.front() << '\n';
+        }
+        else { // back
+            if (Q.empty()) cout << -1 << '\n';
+            else cout << Q.back() << '\n';
+        }
+    }
+}
